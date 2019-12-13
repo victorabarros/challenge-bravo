27c88f20e542155b8ccfc5586e0c86a5c22ee406
Victor Almeida Barros <42843223+victorabarros@users.noreply.github.com>
2019-12-12 23:44:23 -0300

    Add sudo to commands

--- up_application.sh
+++ up_application.sh
@@ -1,8 +1,8 @@
-docker rm -f hurb-challenge
-docker rmi -f hurb-image:1.0
-docker build -t hurb-image:1.0 .
-docker run -d -p 5001:5000 --name hurb-challenge hurb-image:1.0
-docker image ls -a
-docker ps -a
+sudo docker rm -f hurb-challenge
+sudo docker rmi -f hurb-image:1.0
+sudo docker build -t hurb-image:1.0 .
+sudo docker run -d -p 5001:5000 --name hurb-challenge hurb-image:1.0
+sudo docker image ls -a
+sudo docker ps -a
 sleep 1
 curl -X GET http://localhost:5001/healthcheck
