76ba1287391ef94ffc364937131c0658a84695bc
Victor Almeida Barros <42843223+victorabarros@users.noreply.github.com>
2019-12-12 23:41:31 -0300

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,17 @@
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo docker run hello-world
